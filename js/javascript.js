1f741a1e48edd6b701c4156fa73dcf3d631bac70
Philipp Otto <philotto95@gmail.com>
2022-01-23 14:16:09 +0100

    Number buttons populate display

--- js/javascript.js
+++ js/javascript.js
@@ -14,6 +14,37 @@ function divide(a,b) {
     return a/b;
 }
 
+function skip(a,b) {
+    return b;
+}
+
 function operate(numberOne, numberTwo, operation) {
     return operation(numberOne, numberTwo);
 }
+
+function populateContent(input) {
+    const content = document.querySelector('.content');
+    content.textContent = input;
+}
+
+function addButtonLogicNumbers() {
+    const buttons = document.querySelectorAll('.number');
+    buttons.forEach(button => button.addEventListener('click', pressNumber));
+}
+
+function changeCurrentDisplay(number) {
+    currentDisplay = currentDisplay + number;
+}
+
+function pressNumber(event) {
+    const buttonNumber = event.target;
+    changeCurrentDisplay(buttonNumber.textContent);
+    populateContent(currentDisplay);
+}
+
+let currentDisplay = "";
+let firstNumber = 0;
+let secondNumber = 0;
+let operation = skip;
+
+addButtonLogicNumbers();
